01f442abd5dc6bd3fe38a85d3fd09fabd5cadee0
Juan Carlos Garcés <juan.garces@marianaguadalupe.com>
2023-03-29 16:48:13 -0500

    Reestructuración de toda la página y su código

---
+++ main.js
@@ -0,0 +1,34 @@
+var nombre = "Andrea";
+console.log(nombre);
+//Esto es un comentario en JS
+
+//String
+var cadena="Andrea";
+cadena = 'Andrea "Andy" Sánchez';
+cadena = "Andrea 'Andy' Sánchez";
+cadena = "Andrea \"Andy\" Sánchez";
+console.log(cadena);
+
+//Number
+var numero = 15;
+numero = 0.012
+numero = -0.012
+numero = 2.2e+20
+numero = Infinity
+numero = NaN
+console.log(numero);
+
+//Boolean
+var activo = true;
+activo = false;
+console.log(activo);
+
+//Undefined
+var indefinido;
+console.log(indefinido);
+
+//Null
+var nulo = null;
+console.log(nulo);
+
+
